1523933b48ff451abbf53648c76de1828c17396e
Carina07 <carina.kramberger@s.hakmistelbach.ac.at>
2019-04-30 13:42:38 +0200

    Klasse richtiggestellt

--- Speiseplanprojekt - Carina Manuel/Speiseplanprojekt - Carina Manuel/Speisen.cs
+++ Speiseplanprojekt - Carina Manuel/Speiseplanprojekt - Carina Manuel/Speisen.cs
@@ -6,27 +6,39 @@ using System.Threading.Tasks;
 
 namespace Speiseplanprojekt___Carina_Manuel
 {
-    class Speisen
+    public class Speisen
     {
         protected string sID;
+        protected string speisenart;
         protected string name;
         protected string zusatz;
+        protected string fleischart;
         protected string veg;
         protected string wk;
+        protected string kinderspeise;
 
-        public Speisen(string SID, string name, string zusatz, string veg, string wk)
+        public Speisen(string SID, string speisenart,  string name, string zusatz, string fleischart, string veg, string wk, string kinderspeise)
         {
             this.sID = SID;
+            this.speisenart = speisenart;
             this.name = name;
             this.zusatz = zusatz;
+            this.fleischart = fleischart;
             this.veg = veg;
             this.wk = wk;
+            this.kinderspeise = kinderspeise;
         }
 
         public string SID
         {
             get { return sID; }
             set { sID = value; }
+        }
+
+        public string Speisenart
+        {
+            get { return speisenart; }
+            set { speisenart = value; }
         }
 
         public string Name
@@ -40,6 +52,11 @@ namespace Speiseplanprojekt___Carina_Manuel
             get { return zusatz; }
             set { zusatz = value; }
         }
+        public string Fleischart
+        {
+            get { return fleischart; }
+            set { fleischart = value; }
+        }
 
         public string Veg
         {
@@ -52,10 +69,15 @@ namespace Speiseplanprojekt___Carina_Manuel
             get { return wk; }
             set { wk = value; }
         }
+        public string Kinderspeise
+        {
+            get { return kinderspeise; }
+            set { kinderspeise = value; }
+        }
 
         public override string ToString()
         {
-            return sID + " " +  name + "\t\t" + zusatz + "\t" + veg + "\t\t" + wk;
+            return sID + "\t" + speisenart + "\t"  + name + "\t\t" + zusatz + "\t"+fleischart + "\t" + "\t" + veg + "\t\t" + wk + "\t" + kinderspeise;
         }
     }
 }
